2b37422ed3d1e177a09e4a89f23f89f744bd195c
Szymon <piersiak.szymon@gmail.com>
2021-06-03 17:57:41 +0200

    Creating a DbContext

---
+++ LibraryApp/LibraryApp.API/Data/LibraryDbContext.cs
@@ -0,0 +1,26 @@
+﻿using LibraryApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryApp.API.Data
+{
+    public class LibraryDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Book> Books { get; set; }
+        public DbSet<CopiesOfTheBook> CopiesOfTheBooks { get; set; }
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Publisher> Publishers { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Reservation> Reservations { get; set; }
+        public DbSet<Borrowed> Borroweds { get; set; }
+
+        public LibraryDbContext(DbContextOptions<LibraryDbContext> dbContextOptions) : base(dbContextOptions)
+        {
+
+        }
+    }
+}
