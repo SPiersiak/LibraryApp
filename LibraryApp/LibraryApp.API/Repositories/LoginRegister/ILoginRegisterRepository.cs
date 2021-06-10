696954631e7c41d998284d9f771de3e938b57c98
Szymon <piersiak.szymon@gmail.com>
2021-06-10 13:41:13 +0200

    adding a repository for login and registration actions

---
+++ LibraryApp/LibraryApp.API/Repositories/LoginRegister/ILoginRegisterRepository.cs
@@ -0,0 +1,14 @@
+﻿using LibraryApp.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryApp.API.Repositories.LoginRegister
+{
+    public interface ILoginRegisterRepository
+    {
+        Task<User> LoginAsync(string userName, string password);
+        Task<bool> RegisterAsync(string firstName, string lastName, string email, string userName, string password);
+    }
+}
---
+++ LibraryApp/LibraryApp.API/Repositories/LoginRegister/LoginRegisterRepository.cs
@@ -0,0 +1,72 @@
+﻿using LibraryApp.API.Data;
+using LibraryApp.API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryApp.API.Repositories.LoginRegister
+{
+    public class LoginRegisterRepository : ILoginRegisterRepository
+    {
+        private readonly LibraryDbContext _libraryDbContext;
+
+        public LoginRegisterRepository(LibraryDbContext libraryDbContext)
+        {
+            _libraryDbContext = libraryDbContext;
+        }
+
+        public async Task<User> LoginAsync(string userName, string password)
+        {
+            var user = await _libraryDbContext.Users.Where(x => x.UserName == userName).FirstOrDefaultAsync();
+            if(user == null)
+            {
+                throw new Exception("Błąd logowania");
+            }
+            if(user.Password != password)
+            {
+                throw new Exception("Błąd logowania");
+            }
+            return new User()
+            {
+                UserID = user.UserID,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Email = user.Email,
+                UserName = user.UserName,
+                Password = user.Password,
+                IsEmployee = user.IsEmployee
+            };
+        }
+
+        public async Task<bool> RegisterAsync(string firstName, string lastName, string email, string userName, string password)
+        {
+            var user = await _libraryDbContext.Users.Where(x => x.UserName == userName).FirstOrDefaultAsync();
+            if(user != null)
+            {
+                return false;
+                throw new Exception("Jest juz taki user");
+            }
+            if(String.IsNullOrEmpty(firstName) || String.IsNullOrEmpty(lastName) || String.IsNullOrEmpty(email) || String.IsNullOrEmpty(password))
+            {
+                return false;
+                throw new Exception("Wymagane pola nie są uzupełnine");
+            }
+            var newUser = new User()
+            {
+                FirstName = firstName,
+                LastName = lastName,
+                UserName = userName,
+                Email = email,
+                Password = password,
+                IsEmployee = false
+            };
+
+            await _libraryDbContext.Users.AddAsync(newUser);
+            await _libraryDbContext.SaveChangesAsync();
+            return true;
+        }
+    }
+}
