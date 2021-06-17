18719e23bb6c9a8f7530e6a9f8fad8b5d8759587
Szymon <piersiak.szymon@gmail.com>
2021-06-17 11:04:59 +0200

    adding new project for mobile app
    xamarin.forms app for android

---
+++ LibraryApp/LibraryApp.Mobile/LibraryApp.Mobile/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using LibraryApp.Mobile.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace LibraryApp.Mobile.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
