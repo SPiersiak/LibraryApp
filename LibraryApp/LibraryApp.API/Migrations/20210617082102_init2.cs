14525f5ec8b56ec4664ae20bf60e5a8247eda007
Szymon <piersiak.szymon@gmail.com>
2021-06-17 10:59:11 +0200

    add new migration

---
+++ LibraryApp/LibraryApp.API/Migrations/20210617082102_init2.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LibraryApp.API.Migrations
+{
+    public partial class init2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ReservationStart",
+                table: "Reservations",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(TimeSpan),
+                oldType: "time");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "ReservationEnd",
+                table: "Reservations",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(TimeSpan),
+                oldType: "time");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Reservations",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Reservations");
+
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "ReservationStart",
+                table: "Reservations",
+                type: "time",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "ReservationEnd",
+                table: "Reservations",
+                type: "time",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+    }
+}
