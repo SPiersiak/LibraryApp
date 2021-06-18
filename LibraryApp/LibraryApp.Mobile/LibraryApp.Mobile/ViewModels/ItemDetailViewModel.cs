abc1e389a81f1d8c399cbe25070f0038f7c948fb
Szymon <piersiak.szymon@gmail.com>
2021-06-19 00:42:35 +0200

    adding a page displaying the details of the selected book

--- LibraryApp/LibraryApp.Mobile/LibraryApp.Mobile/ViewModels/ItemDetailViewModel.cs
+++ LibraryApp/LibraryApp.Mobile/LibraryApp.Mobile/ViewModels/ItemDetailViewModel.cs
@@ -1,4 +1,8 @@
 ﻿using LibraryApp.Mobile.Models;
+using LibraryApp.Mobile.Services.Authors;
+using LibraryApp.Mobile.Services.BookService;
+using LibraryApp.Mobile.Services.Categories;
+using LibraryApp.Mobile.Services.Publishers;
 using System;
 using System.Diagnostics;
 using System.Threading.Tasks;
@@ -6,18 +10,42 @@ using Xamarin.Forms;
 
 namespace LibraryApp.Mobile.ViewModels
 {
-    [QueryProperty(nameof(ItemId), nameof(ItemId))]
+    [QueryProperty(nameof(BookId), nameof(BookId))]
     public class ItemDetailViewModel : BaseViewModel
     {
-        private string itemId;
-        private string text;
+        public IBookService _bookService => DependencyService.Get<IBookService>();
+        public IAuthorService _authorService => DependencyService.Get<IAuthorService>();
+        public IPublisherService _publisherService => DependencyService.Get<IPublisherService>();
+        public ICategoryService _categoryService => DependencyService.Get<ICategoryService>();
+        private long bookId;
+        private string title;
         private string description;
-        public string Id { get; set; }
+        private string authorName;
+        private string category;
+        private string publisher;
+        private string isbn;
+        private string date;
+        public long Id { get; set; }
 
         public string Text
         {
-            get => text;
-            set => SetProperty(ref text, value);
+            get => title;
+            set => SetProperty(ref title, value);
+        }
+        public string ISBN
+        {
+            get => isbn;
+            set => SetProperty(ref isbn, value);
+        }
+        public string Date
+        {
+            get => date;
+            set => SetProperty(ref date, value);
+        }
+        public string AuthorName
+        {
+            get => authorName;
+            set => SetProperty(ref authorName, value);
         }
 
         public string Description
@@ -25,28 +53,47 @@ namespace LibraryApp.Mobile.ViewModels
             get => description;
             set => SetProperty(ref description, value);
         }
+        public string Publisher
+        {
+            get => publisher;
+            set => SetProperty(ref publisher, value);
+        }
+        public string Category
+        {
+            get => category;
+            set => SetProperty(ref category, value);
+        }
 
-        public string ItemId
+        public long BookId
         {
             get
             {
-                return itemId;
+                return bookId;
             }
             set
             {
-                itemId = value;
+                bookId = value;
                 LoadItemId(value);
             }
         }
 
-        public async void LoadItemId(string itemId)
+        public async void LoadItemId(long itemId)
         {
             try
             {
-                var item = await DataStore.GetItemAsync(itemId);
-                Id = item.Id;
-                Text = item.Text;
+                
+                var item = await _bookService.GetBookById(itemId);
+                var author = await _authorService.GetAuthor(item.AuthorId);
+                var pub = await _publisherService.GetPublisherById(item.PublisherId);
+                var cate = await _categoryService.GetCategoryById(item.CategoryId);
+                AuthorName = author.FirstName + " " + author.LastName;
+                Id = item.BookId;
+                Text = item.BookName;
                 Description = item.Description;
+                Publisher = pub.PublisherName;
+                Category = cate.CategoryName;
+                ISBN = item.ISBN;
+                Date = item.ReleaseDate.ToString();
             }
             catch (Exception)
             {
