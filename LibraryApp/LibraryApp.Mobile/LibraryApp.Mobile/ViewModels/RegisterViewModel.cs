2bc6e5b6e7894bece0296bb2923801bebc8cbf1a
Szymon <piersiak.szymon@gmail.com>
2021-06-19 22:18:49 +0200

    adding register page

---
+++ LibraryApp/LibraryApp.Mobile/LibraryApp.Mobile/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,88 @@
+﻿using LibraryApp.Mobile.Services.Authentication;
+using LibraryApp.Mobile.Services.Authentication.DTO;
+using LibraryApp.Mobile.Views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace LibraryApp.Mobile.ViewModels
+{
+    public class RegisterViewModel : BaseViewModel
+    {
+        public IAuthenticationService _authenticationService => DependencyService.Get<IAuthenticationService>();
+        public Command RegisterCommand { get; }
+        public Command CancelCommand { get; }
+        private string firstName;
+        private string lastName;
+        private string userName;
+        private string email;
+        private string password;
+        public RegisterViewModel()
+        {
+            RegisterCommand = new Command(Register,ValidateSave);
+            this.PropertyChanged +=
+                (_, __) => RegisterCommand.ChangeCanExecute();
+        }
+        public string FirstName
+        {
+            get => firstName;
+            set => SetProperty(ref firstName, value);
+        }
+        public string LastName
+        {
+            get => lastName;
+            set => SetProperty(ref lastName, value);
+        }
+        public string UserName
+        {
+            get => userName;
+            set => SetProperty(ref userName, value);
+        }
+        public string Email
+        {
+            get => email;
+            set => SetProperty(ref email, value);
+        }
+        public string Password
+        {
+            get => password;
+            set => SetProperty(ref password, value);
+        }
+        private bool ValidateSave()
+        {
+            return !String.IsNullOrWhiteSpace(firstName)
+                && !String.IsNullOrWhiteSpace(lastName)
+                && !String.IsNullOrWhiteSpace(userName)
+                && !String.IsNullOrWhiteSpace(email)
+                && !String.IsNullOrWhiteSpace(password);
+        }
+        private void Clear()
+        {
+            FirstName = "";
+            LastName = "";
+            UserName = "";
+            Email = "";
+            Password = "";
+        }
+        private async void Register()
+        {
+            RegisterDto registerDto = new RegisterDto()
+            {
+                FirstName = firstName,
+                LastName = lastName,
+                UserName = userName,
+                Email = email,
+                Password = password
+            };
+            var result = await _authenticationService.Register(registerDto);
+            if(result)
+                await Shell.Current.GoToAsync($"//{nameof(LoginPage)}");
+            else
+            {
+                Clear();
+                await Application.Current.MainPage.DisplayAlert("Błąd", "Istnieje użytkownik z takim loginem", "OK");
+            }
+        }
+    }
+}
---
+++ LibraryApp/LibraryApp.Mobile/LibraryApp.Mobile/Views/RegisterPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using LibraryApp.Mobile.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LibraryApp.Mobile.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RegisterPage : ContentPage
+    {
+        public RegisterPage()
+        {
+            InitializeComponent();
+            BindingContext = new RegisterViewModel();
+        }
+        private async void Cancel_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopAsync();
+        }
+    }
+}
