76fe6dda05590140184077ba0db724a74975e280
Szymon <piersiak.szymon@gmail.com>
2021-06-20 01:33:59 +0200

    adding user service

---
+++ LibraryApp/LibraryApp.Mobile/LibraryApp.Mobile/Services/UserServices/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using LibraryApp.Mobile.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryApp.Mobile.Services.UserServices
+{
+    public interface IUserService
+    {
+        Task<bool> UpdateUserData(User user);
+    }
+}
---
+++ LibraryApp/LibraryApp.Mobile/LibraryApp.Mobile/Services/UserServices/UserService.cs
@@ -0,0 +1,21 @@
+﻿using LibraryApp.Mobile.Models;
+using LibraryApp.Mobile.Services.Request;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace LibraryApp.Mobile.Services.UserServices
+{
+    public class UserService : IUserService
+    {
+        public IRequestService _requestService => DependencyService.Get<IRequestService>();
+        public async Task<bool> UpdateUserData(User user)
+        {
+            var uri = $"https://192.168.0.107:45456/api/User";
+            var result = await _requestService.PutAsync<User>(uri, user);
+            return true;
+        }
+    }
+}
